5c7fc49f98ffe11154c3b2652ec3a776d31b54dd
khazzy999 <khazzybillion@gmail.com>
2025-11-25 10:55:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
-from flask import Flask, request
-import telebot
 import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-bot = telebot.TeleBot(BOT_TOKEN)
+from flask import Flask, request
+from bot import bot
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route('/')
 def home():
-    return "Bot is running on Render!"
+    return "Bot is running!"
 
-@app.route("/webhook", methods=["POST"])
+@app.route('/webhook', methods=['POST'])
 def webhook():
-    json_str = request.get_data().decode("utf-8")
-    update = telebot.types.Update.de_json(json_str)
-    bot.process_new_updates([update])
-    return "ok", 200
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    return 'OK'
 
-from bot import *
+if __name__ == "__main__":
+    if os.environ.get('RENDER'):  # Render productionda
+        bot.remove_webhook()
+        bot.set_webhook(f"https://{os.environ.get('RENDER_EXTERNAL_HOSTNAME')}/webhook")
+    else:  # Localda
+        bot.polling(none_stop=True)
