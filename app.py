9fb206fadbd006286c75f9c24d2a0ba40b9ddf8f
khazzy999 <khazzybillion@gmail.com>
2025-11-25 11:14:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,18 @@
-import os
 from flask import Flask, request
-from bot import bot
+import telebot
+import os
+from bot import bot  # bot.py import qilindi
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/", methods=["GET"])
 def home():
-    return "Bot is running!"
+    return "Bot ishlayapti!"
 
-@app.route('/webhook', methods=['POST'])
+@app.route("/webhook", methods=["POST"])
 def webhook():
-    if request.headers.get('content-type') == 'application/json':
-        json_string = request.get_data().decode('utf-8')
-        update = telebot.types.Update.de_json(json_string)
-        bot.process_new_updates([update])
-        return ''
-    return 'OK'
-
-if __name__ == "__main__":
-    if os.environ.get('RENDER'):  # Render productionda
-        bot.remove_webhook()
-        bot.set_webhook(f"https://{os.environ.get('RENDER_EXTERNAL_HOSTNAME')}/webhook")
-    else:  # Localda
-        bot.polling(none_stop=True)
+    json_str = request.get_data().decode("utf-8")
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "ok", 200
